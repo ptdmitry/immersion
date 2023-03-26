63e5abf732e00950167e8684a7b163c778d75832
Dmitry <93381648+ptdmitry@users.noreply.github.com>
2023-03-26 10:07:04 +0500

    Update task_14_matrix.py

--- task_14_matrix.py
+++ task_14_matrix.py
@@ -29,19 +29,3 @@ class Matrix:
 
     def __lt__(self, other):
         return self.matrix < other.matrix
-
-
-if __name__ == '__main__':
-    m1 = Matrix(([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
-    m2 = Matrix(([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
-    m3 = m1 + m2
-    print(m1 + m2)
-    print(m1)
-    # print(m2)
-    # print(m3)
-    # print(m1 == m2)
-    # print(m1 == m3)
-    # print(m1 > m2)
-    # print(m3 > m2)
-    # print(m1 < m2)
-    # print(m1 < m3)
